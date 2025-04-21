ae3daf47818e4bb8e575ef4ecc824224cf882a7f
anaysah <ajay.kumarsahu@oodles.io>
2025-04-21 15:22:42 +0530

    feat: post thumbnail image upload support
    testcase in TestPostViewset for image upload
    media settings in settings and urls file

--- backend/blogs/tests.py
+++ backend/blogs/tests.py
@@ -5,6 +5,19 @@ from users.models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Category
 
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+import tempfile
+
+def generate_test_image():
+    # Create an in-memory image
+    image = Image.new('RGB', (100, 100), color='red')
+    temp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+    image.save(temp_file, format='JPEG')
+    temp_file.seek(0)
+    return SimpleUploadedFile('test.jpg', temp_file.read(), content_type='image/jpeg')
+
+
 # Create your tests here.
 class CategoryViewTest(APITestCase):
     def setUp(self):
@@ -44,13 +57,13 @@ class CategoryViewTest(APITestCase):
 class PostViewTest(APITestCase):
     def setUp(self):
         self.url = reverse('post-list')
-        category = Category.objects.create(name="Test Category")
+        self.category = Category.objects.create(name="Test Category")
         self.post = {
             "title": "Test Post",
             "snippet": "Test Snippet",
             "body": "Test Body Content lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet",
             "status": "published",
-            "category_id": category.id
+            "category_id": self.category.id
         }
 
         self.user = CustomUser.objects.create_user(
@@ -62,6 +75,37 @@ class PostViewTest(APITestCase):
 
     def test_create_post(self):
         response = self.client.post(self.url, self.post)
+        # print(response)
+        # print(response.data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_post_with_invalid_category(self):
+        invalid_post = {
+            "title": "Invalid Post",
+            "snippet": "Invalid Snippet",
+            "body": "Invalid Body Content",
+            "status": "published",
+            "category_id": 999  # Invalid category ID
+        }
+        response = self.client.post(self.url, invalid_post)
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_post_unauthenticated(self):
+        self.client.credentials()
+        response = self.client.post(self.url, self.post)
+        self.assertEqual(response.status_code, 401)
+    
+    def test_create_post_with_thumbnail(self):
+        image = generate_test_image()
+        post_data = {
+            "title": "Post with Image",
+            "snippet": "Image test",
+            "body": "Body here... lorem ipsum dolor sit amet lorem ipsum 100 times lorem ipsum dolor sit amet  lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet", 
+            "category_id": self.category.id,
+            "thumbnail": image
+        }
+        response = self.client.post(self.url, post_data, format='multipart')
         print(response)
         print(response.data)
         self.assertEqual(response.status_code, 201)
+        self.assertIn('thumbnail', response.data)
