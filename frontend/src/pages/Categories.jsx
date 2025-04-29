4a381ee2f045c3c05e125cb56e026c2742165979
anaysah <ajay.kumarsahu@oodles.io>
2025-04-29 16:01:20 +0530

    feat: api integration Categories

--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const CategoryBox = ({ name, svg }) => {
+  console.log('SVG Content:', svg);
     return (
       <div className="flex flex-col items-center justify-center p-4  rounded-md bg-back-1 px-20">
         <div
@@ -17,14 +20,36 @@ export const CategoryBox = ({ name, svg }) => {
 
 
 function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get('/api/blogs/categories/');
+        setCategories(response.data.results);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+  
+
     return (
       <div className="container mx-auto p-4">
         <h1 className="text-3xl font-bold text-center text-[27px] text-text-4 mb-5">Categories</h1>
         <div className='flex justify-center flex-wrap gap-4'>
-            <CategoryBox name="Travel" svg={`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-icon lucide-map"><path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"/><path d="M15 5.764v15"/><path d="M9 3.236v15"/></svg>` }/>
-            <CategoryBox name="Food" svg={`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cookie-icon lucide-cookie"><path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5"/><path d="M8.5 8.5v.01"/><path d="M16 15.5v.01"/><path d="M12 12v.01"/><path d="M11 17v.01"/><path d="M7 14v.01"/></svg>` }/>
-            <CategoryBox name="Sports" svg={`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-medal-icon lucide-medal"><path d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"/><path d="M11 12 5.12 2.2"/><path d="m13 12 5.88-9.8"/><path d="M8 7h8"/><circle cx="12" cy="17" r="5"/><path d="M12 18v-2h-.5"/></svg>` }/>
+          {categories.map((category) => (
+            <CategoryBox
+              key={category.id}
+              name={category.name}
+              svg={category.icon_svg}
+            />
+          ))}
         </div>
+
       </div>
     );
   }
